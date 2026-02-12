3d28e5460b37e0d14ca1c6f679920e9fbafdebff
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-12 20:37:38 +0100

    Set jwt configuration in server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 'use strict';
 
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const { Pool } = require('pg');
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
-const postRoutes = require('./routes/postRoutes'); 
+const postRoutes = require('./routes/postRoutes');
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Local Postgres connection pool
 const pool = new Pool({
@@ -29,8 +34,29 @@ const init = async () => {
         }
     });
 
+    await server.register(Jwt);
+
+      server.auth.strategy('jwt', 'jwt', {
+    keys: JWT_SECRET,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: 3600
+    },
+    validate: async (artifacts, request, h) => {
+      return {
+        isValid: true,
+        credentials: artifacts.decoded.payload
+      };
+    }
+  });
+
+  server.auth.default('jwt');
+
     // Register routes
     server.route(postRoutes(pool));
+    server.route(authRoutes(pool));
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
